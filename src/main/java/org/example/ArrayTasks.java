b580d3227b607e69eb6395412b0694e6fc021a5a
Nadya <shashinadya@gmail.com>
2022-12-05 23:54:17 -0800

    Committed added Arrays tasks

--- src/main/java/org/example/ArrayTasks.java
+++ src/main/java/org/example/ArrayTasks.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Random;
 
 public class ArrayTasks {
     public static void main(String[] args) {
@@ -9,7 +10,12 @@ public class ArrayTasks {
         // task2();
         //task3();
         //task6();
-        task6Variant2();
+        //task6Variant2();
+        //task8();
+        //task9();
+        //task10();
+        //task19();
+        task20();
     }
 
     //1. Заполнить массив нулями, кроме первого и последнего элементов, которые должны быть равны единице.
@@ -50,6 +56,7 @@ public class ArrayTasks {
         }
         System.out.println(Arrays.toString(array));
     }
+
     public static void task6Variant2() {
         int[] array = new int[7];
         int value = array.length * 3;
@@ -58,5 +65,62 @@ public class ArrayTasks {
             value -= 3;
         }
         System.out.println(Arrays.toString(array));
+    }
+
+    //8. Заполнить массив заданной длины различными простыми числами.
+    public static void task8() {
+        int[] array = new int[7];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    //9. Создать массив, каждый элемент которого равен квадрату своего номера.
+    public static void task9() {
+        int[] array = new int[7];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.pow(i, 2);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    //10. Создать массив, на четных местах в котором стоят единицы, а на нечетных местах - числа,
+    // равные остатку от деления своего номера на 5.
+    public static void task10() {
+        int[] array = new int[15];
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = i % 5;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    //19. Определить, содержит ли массив данное число x
+    public static void task19() {
+        int x = 8;
+        int[] array = new int[]{1, 7, 6, 8, 0, 5, 4};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 8) {
+                System.out.println("В массиве есть число 8");
+                return;
+            }
+        }
+        System.out.println("В массиве нет числа 8");
+    }
+
+    //20. Найти количество четных чисел в массиве.
+    public static void task20() {
+        int countOfEvenNumbers = 0;
+        int[] array = new int[]{5, 66, 8, 4, 3, 9, 2, 54, 9};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                countOfEvenNumbers += 1;
+            }
+        }
+        System.out.println(countOfEvenNumbers);
     }
 }
